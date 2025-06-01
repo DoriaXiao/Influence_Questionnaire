ca32811284715eafb6c7250d1af8d9a9fd7f9fa4
Xingyao (Doria) Xiao <77747786+DoriaXiao@users.noreply.github.com>
2025-06-01 09:36:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -249,7 +249,7 @@ def page_summary():
 # --- Handle restart request ---
 if st.session_state.get('restart_requested', False):
     restart_sequence()
-    st.experimental_rerun()
+    raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
 
 # --- Page: Thank You ---
 def page_thank_you():
