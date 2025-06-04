bbc1f79aed24a55df446fa3febc9d42b78eb7b22
Xingyao (Doria) Xiao <77747786+DoriaXiao@users.noreply.github.com>
2025-06-04 09:16:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 # Run this app with: streamlit run influence_scoring_app.py
 # Deploy it on Streamlit Cloud by pushing to GitHub and connecting your repo at https://streamlit.io/cloud
 # SHEET_URL = "https://script.google.com/macros/s/AKfycbxM59uBQ5kQ_08-E81gzoOvHGZAYzEzGfp_6jpCZXxXJBNT-KVOV8e8rHtNdnVtDiO1ZA/exec"
-pip install streamlit-authenticator
+
 import streamlit as st
 import streamlit_authenticator as stauth
 import pandas as pd
