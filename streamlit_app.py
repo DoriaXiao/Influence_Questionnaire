e325fdfff17faf1e643f889beea0699c02abd029
Xingyao (Doria) Xiao <77747786+DoriaXiao@users.noreply.github.com>
2025-06-04 08:31:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@
 # Deploy it on Streamlit Cloud by pushing to GitHub and connecting your repo at https://streamlit.io/cloud
 # SHEET_URL = "https://script.google.com/macros/s/AKfycbxM59uBQ5kQ_08-E81gzoOvHGZAYzEzGfp_6jpCZXxXJBNT-KVOV8e8rHtNdnVtDiO1ZA/exec"
 
-!pip install streamlit-authenticator
-
 from streamlit_authenticator import Hasher, Authenticate
 
 import streamlit as st
