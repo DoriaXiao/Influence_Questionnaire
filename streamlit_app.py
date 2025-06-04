2e225d3bebbaae79e9d106cf11f23041861b53df
Xingyao (Doria) Xiao <77747786+DoriaXiao@users.noreply.github.com>
2025-06-04 09:17:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ import json
 import yaml
 from yaml.loader import SafeLoader
 
-with open('Influence_Questionnaire/config.yaml') as file:
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
