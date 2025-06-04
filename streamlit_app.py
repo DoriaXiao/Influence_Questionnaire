7bcc9cd8c51f87a889d1379a0f7ca95f30fb81f5
Xingyao (Doria) Xiao <77747786+DoriaXiao@users.noreply.github.com>
2025-06-04 09:14:20 -0700

    Update streamlit_app.py: google login 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,39 @@
 # Run this app with: streamlit run influence_scoring_app.py
 # Deploy it on Streamlit Cloud by pushing to GitHub and connecting your repo at https://streamlit.io/cloud
 # SHEET_URL = "https://script.google.com/macros/s/AKfycbxM59uBQ5kQ_08-E81gzoOvHGZAYzEzGfp_6jpCZXxXJBNT-KVOV8e8rHtNdnVtDiO1ZA/exec"
-
+pip install streamlit-authenticator
 import streamlit as st
+import streamlit_authenticator as stauth
 import pandas as pd
 from datetime import date, datetime
 import os
 import requests
 import json
+import yaml
+from yaml.loader import SafeLoader
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Pre-hashing all plain text passwords once
+# stauth.Hasher.hash_passwords(config['credentials'])
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+try:
+    authenticator.experimental_guest_login('Login with Google',
+                                           provider='google',
+                                           oauth2=config['oauth2'])
+    authenticator.experimental_guest_login('Login with Microsoft',
+                                           provider='microsoft',
+                                           oauth2=config['oauth2'])
+except Exception as e:
+    st.error(e)
 
 st.set_page_config(page_title="Influence Scoring App", layout="wide")
 
